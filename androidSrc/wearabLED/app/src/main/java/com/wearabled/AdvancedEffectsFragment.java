efc8ea549057b18c03091d3cd40edc9e112ce028
Patrick Rehm <tapdingo@gmail.com>
2014-12-27 16:31:46 +0100

    Beat detection basics continued....

--- androidSrc/wearabLED/app/src/main/java/com/wearabled/AdvancedEffectsFragment.java
+++ androidSrc/wearabLED/app/src/main/java/com/wearabled/AdvancedEffectsFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton;
+import android.util.Log;
 import android.media.AudioRecord;
 
 import org.jtransforms.fft.DoubleFFT_1D;
@@ -20,6 +23,25 @@ public class AdvancedEffectsFragment  extends Fragment {
         View rootView = inflater.inflate(
                 R.layout.fragment_advanced, container, false);
 
+        ToggleButton beatOn = (ToggleButton) rootView.findViewById(R.id.beatDetectorButton);
+        beatOn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    enableBeatDetection();
+                } else {
+                    disableBeatDetection();
+                }
+            }
+        });
+
         return rootView;
+    }
+
+    private void enableBeatDetection() {
+        Log.d ("Advanced Effects", "Trying to enable beat detection...");
+    }
+
+    private void disableBeatDetection() {
+        Log.d ("Advanced Effects", "Disabling beat detection...");
     }
 }
